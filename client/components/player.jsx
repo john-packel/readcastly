34e95a8a016d16f02cf470cb7de25cbed897fc53
John Packel <john.packel@gmail.com>
2017-04-20 15:14:06 -0400

    Reverted code from prior commits.

--- client/components/player.jsx
+++ client/components/player.jsx
@@ -5,9 +5,9 @@ const Player = ({track}) => (
   <div className="audio-player">
   {
     (track.url === null)
-    ? <AudioPlayer playlist={[{url: 'https://s3.amazonaws.com/readcastly-user-files/king-now-is-the-perfect-1303-joanna.mp3', displayText: 'KING: Now is the perfect time to discuss how and why Bernie Sanders could\'ve beaten Donald Trump'}, {url: 'https://s3.amazonaws.com/readcastly-user-files/the-white-houses-misleading-statements-1304-salli.mp3', displayText: 'The White House’s misleading statements about Trump’s ‘armada’ heading to North Korea'}]} hideBackSkip={true} style={{ position: 'fixed', bottom: 0 }} />
+    ? <AudioPlayer autoplay cycle={false} playlist={[{url: 'https://s3.amazonaws.com/readcastly-user-files/readcastly-welcome-part01.mp3', displayText: 'Welcome to ReadCast.ly!'}, {url: 'https://s3.amazonaws.com/readcastly-user-files/readcastly-welcome-part02.mp3', displayText: 'Welcome to ReadCast.ly (continued)'}]} hideBackSkip={true} style={{ position: 'fixed', bottom: 0 }} />
 
-    : <AudioPlayer playlist={[{url: track.url, displayText: track.title}]} hideBackSkip={true} style={{ position: 'fixed', bottom: 0 }} />
+    : <AudioPlayer autoplay cycle={false} playlist={[{url: track.url, displayText: track.title}]} hideBackSkip={true} style={{ position: 'fixed', bottom: 0 }} />
   }
   </div>
 );
